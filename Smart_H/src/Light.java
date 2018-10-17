e5e03724353f867d3abc306b4967ad8e61e8e68d
Sophie <sophie.schorochoff@student.uclouvain.be>
2018-10-17 08:56:54 +0200

    Light with names

--- Smart_H/src/Light.java
+++ Smart_H/src/Light.java
@@ -1,16 +1,20 @@
 public class Light {
 
-
+    String name;
     public boolean on = false;
+
+    public Light(String name) {
+        this.name = name;
+    }
 
     public void turn_on(){
         on = true;
-        System.out.println("Light on !");
+        System.out.println(name + "Light on !");
     }
 
     public void turn_off(){
         on = false;
-        System.out.println("Light off !");
+        System.out.println(name + "Light off !");
     }
 
 
