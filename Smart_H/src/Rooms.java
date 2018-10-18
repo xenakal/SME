b2955640f78e1a659b21d93d8941b3d191eb17df
xenakal <alexandros.xenakis@student.uclouvain.be>
2018-10-18 10:38:03 +0200

    Classe Rooms non remplie ajoutée

---
+++ Smart_H/src/Rooms.java
@@ -0,0 +1,13 @@
+public class Rooms {
+
+
+    private MotionDetector light_sensor;
+    private Light living_light;
+
+    public Rooms(MotionDetector[] sensor, Light[] light){
+        this.light_sensor = sensor;
+        this.living_light = light;
+
+    }
+
+}
